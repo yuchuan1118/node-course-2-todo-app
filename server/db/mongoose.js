bd3e3e85cbd5f760e63bf398d284f93feb1916de
yuchuan1118 <yuchuan1118@gmail.com>
2018-04-15 23:45:37 +0800

    Add POST / todos route and refactor mongoose

---
+++ server/db/mongoose.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+
+
+module.exports = {mongoose};
+// module.exports = {
+//   mongoose: mongoose
+// };
