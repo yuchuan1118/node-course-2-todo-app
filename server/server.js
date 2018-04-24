bfe245e71a1cd3390d960d1d4142f6246ab5e245
yuchuan1118 <yuchuan1118@gmail.com>
2018-04-24 22:23:33 +0800

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -22,6 +22,14 @@ app.post('/todos', (req, res) => {
   });
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.staus(400).send(e);
+  });
+});
+
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
