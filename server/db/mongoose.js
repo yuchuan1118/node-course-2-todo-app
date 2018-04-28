b45cc4e0ef71cb2a28a302a872bb11434333c17a
yuchuan1118 <yuchuan1118@gmail.com>
2018-04-28 12:35:55 +0800

    username fixed

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-const MONGODB_URI = 'mongodb://chrisle:chris@ds161459.mlab.com:61459/todoapp'
+const MONGODB_URI = 'mongodb://chrisli:chris@ds161459.mlab.com:61459/todoapp'
 mongoose.Promise = global.Promise;
 mongoose.connect(MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
