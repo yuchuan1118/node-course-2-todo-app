cd33ea540a57e88b45571bffe0828e2399e9ffde
yuchuan1118 <yuchuan1118@gmail.com>
2018-05-02 23:48:15 +0800

    mLab bug fix trial

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,10 @@
 var mongoose = require('mongoose');
 const MONGODB_URI = 'mongodb://chrisli:chris@ds161459.mlab.com:61459/todoapp'
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI || MONGODB_URI);
+// mongoose.connect(MONGODB_URI);
 // mongoose.connect('mongodb://localhost:27017/TodoApp');
+
 
 module.exports = {mongoose};
 // module.exports = {
